c66f42bfdb50f00b3af3463b52bcac9dd8e6cda4
Wilfox0 <wilfoxlove@gmail.com>
2025-09-23 18:14:23 +0300

    Update supabase.js

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://hetysfxvbbgunqxmcauu.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhldHlzZnh2YmJndW5xeG1jYXV1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgxMjI4MTMsImV4cCI6MjA3MzY5ODgxM30.o8v4wXJOzDkxsrhQQnVFlsaDAwJrdmlvpPwhD74HGdg";
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
