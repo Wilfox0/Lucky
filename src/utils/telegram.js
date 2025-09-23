9423649b644c751e62f88bf01ad9425f3585d23b
Wilfox0 <wilfoxlove@gmail.com>
2025-09-23 19:14:55 +0300

    Update telegram.js

--- src/utils/telegram.js
+++ src/utils/telegram.js
@@ -1,7 +1,8 @@
 // 📌 إرسال إشعارات إلى تليجرام
 export async function sendTelegramMessage(message) {
-  const botToken = "YOUR_BOT_TOKEN";
-  const chatId = "YOUR_CHAT_ID";
+  const botToken = "8351490932:AAGJ7JRAFLCG5g-qo-VHzCP0-vqjL1GVn7w
+";
+  const chatId = "1092353140";
 
   await fetch(`https://api.telegram.org/bot${botToken}/sendMessage`, {
     method: "POST",
