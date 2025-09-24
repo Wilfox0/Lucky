28aee9e46e8ca97ba32e8d1218f1034507e99cd3
Wilfox0 <wilfoxlove@gmail.com>
2025-09-24 23:34:44 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,65 @@
-// src/App.jsx
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import HomePage from "./pages/HomePage";
+
+import Home from "./pages/Home";
 import Products from "./pages/Products";
 import ProductDetails from "./pages/ProductDetails";
 import Cart from "./pages/Cart";
-import About from "./pages/About";
 import Contact from "./pages/Contact";
+import About from "./pages/About";
 import NotFound from "./pages/NotFound";
+import OrderReview from "./pages/OrderReview";
 import AdminLogin from "./pages/AdminLogin";
+
 import AdminLayout from "./pages/admin/AdminLayout";
 import Dashboard from "./pages/admin/Dashboard";
 import ProductsManagement from "./pages/admin/ProductsManagement";
 import CategoriesManagement from "./pages/admin/CategoriesManagement";
+import OrdersManagement from "./pages/admin/OrdersManagement";
 import ShippingManagement from "./pages/admin/ShippingManagement";
 import SocialLinksManagement from "./pages/admin/SocialLinksManagement";
 import SettingsManagement from "./pages/admin/SettingsManagement";
-import OrdersManagement from "./pages/admin/OrdersManagement";
-import OrderReview from "./pages/OrderReview";
 
 function App() {
   return (
     <Router>
       <div className="flex flex-col min-h-screen">
         <Header />
-        <main className="flex-grow">
+        <main className="flex-grow container mx-auto p-4">
           <Routes>
-            <Route path="/" element={<HomePage />} />
+            {/* صفحات عامة */}
+            <Route path="/" element={<Home />} />
             <Route path="/products" element={<Products />} />
-            <Route path="/product/:id" element={<ProductDetails />} />
+            <Route path="/products/:id" element={<ProductDetails />} />
             <Route path="/cart" element={<Cart />} />
-            <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
+            <Route path="/about" element={<About />} />
             <Route path="/order-review" element={<OrderReview />} />
+            <Route path="/admin-login" element={<AdminLogin />} />
 
-            {/* Admin routes */}
-            <Route path="/admin/login" element={<AdminLogin />} />
+            {/* صفحات الأدمن */}
             <Route path="/admin" element={<AdminLayout />}>
               <Route index element={<Dashboard />} />
+              <Route path="dashboard" element={<Dashboard />} />
               <Route path="products" element={<ProductsManagement />} />
               <Route path="categories" element={<CategoriesManagement />} />
-              <Route path="shipping" element={<ShippingManagement />} />
-              <Route path="social-links" element={<SocialLinksManagement />} />
-              <Route path="settings" element={<SettingsManagement />} />
               <Route path="orders" element={<OrdersManagement />} />
+              <Route path="shipping" element={<ShippingManagement />} />
+              <Route path="social" element={<SocialLinksManagement />} />
+              <Route path="settings" element={<SettingsManagement />} />
             </Route>
 
+            {/* لو المسار غلط */}
             <Route path="*" element={<NotFound />} />
           </Routes>
         </main>
         <Footer />
+        <ToastContainer position="bottom-right" />
       </div>
     </Router>
   );
