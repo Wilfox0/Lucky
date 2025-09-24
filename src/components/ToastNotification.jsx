f2a5fa53c759a9da6b0c6f49ed00ca8019e4a55b
Wilfox0 <wilfoxlove@gmail.com>
2025-09-24 23:37:13 +0300

    Update ToastNotification.jsx

--- src/components/ToastNotification.jsx
+++ src/components/ToastNotification.jsx
@@ -1,16 +1,12 @@
 import { toast } from "react-toastify";
 
 const notify = {
-  added: (name) => toast.success(`${name} تمت إضافته للسلة`),
+  addToCart: (name, qty = 1) => toast.success(`${name} تمت إضافته للسلة (${qty})`),
   removed: (name) => toast.info(`${name} تم حذفه من السلة`),
-  cleared: () => toast.warn("تم إفراغ السلة بالكامل"),
-  quantityUpdated: (name, qty) =>
-    toast.info(`تم تحديث ${name} إلى ${qty} قطع`),
-  outOfStock: (name) =>
-    toast.error(`${name} غير متوفر حالياً بالمخزون`),
-  outOfStockLimit: (name, stock) =>
-    toast.error(`${name} أقصى كمية ممكنة ${stock}`), // ✅ هنا أضفت الرسالة اللي انت محتاجها
-  orderConfirmed: () => toast.success("تم تأكيد طلبك بنجاح 🎉"),
+  cartCleared: () => toast.warn("تم إفراغ السلة بالكامل"),
+  quantityUpdated: (name, qty) => toast.info(`تم تحديث ${name} إلى ${qty} قطعة`),
+  outOfStock: (name, stock) => toast.error(`${name} أقصى كمية ممكنة ${stock}`),
+  orderConfirmed: () => toast.success("تم تأكيد الطلب بنجاح!"),
 };
 
 export default notify;
